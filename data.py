db9c0ff127ece5646347f0f58eb1d545b6b7d2c3
mariacsalazar <mcsalazart@gmail.com>
2024-04-23 15:59:06 +0200

    Join data

--- data.py
+++ data.py
@@ -3,25 +3,33 @@ import os
 
 
 def read_files():
-    directorio = os.getcwd()
+    dir = os.getcwd()
 
-    csv_files = [archivo for archivo in os.listdir(directorio) if archivo.endswith('.csv')]
+    csv_files = [file for file in os.listdir(dir) if file.endswith('.csv')]
     dfs = []
     for file in csv_files:
        
         name_parts = file.split('_')
-        ciudad = name_parts[0]
-        tipo = name_parts[1].split('.')[0]  # Eliminar la extensión (.csv)
+        city = name_parts[0]
+        type = name_parts[1].split('.')[0]
         
-        # Leer el archivo CSV
-        path = os.path.join(directorio, file)
+   
+        path = os.path.join(dir, file)
         df = pd.read_csv(path)
         
-        # Agregar dos columnas con la ciudad y el tipo
-        df['city'] = ciudad
-        df['days'] = tipo
+        
+        df['city'] = city
+        df['days'] = type
         dfs.append(df)
     df_final = pd.concat(dfs, ignore_index=True)
 
 
     return df_final.to_parquet("raw_data.parquet", index=False)
+
+
+def main():
+    read_files()
+
+if __name__ == '__main__':
+    main()
+
